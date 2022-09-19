0bc5765591434dfdcf5e1431c887915865289866
maryam karimi <maryamkarimi1376.71@gmail.com>
2022-09-19 13:31:36 +0430

    add prescription and docter classes

---
+++ src/ir/maktab/hw7/modle/Doctor.java
@@ -0,0 +1,8 @@
+package ir.maktab.hw7.modle;
+
+public class Doctor {
+    private String firstName;
+    private String lastName;
+    private String GMCNumber;
+    private String specialist;
+}
---
+++ src/ir/maktab/hw7/modle/Prescription.java
@@ -0,0 +1,10 @@
+package ir.maktab.hw7.modle;
+
+import java.util.Date;
+
+public class Prescription {
+    private Date date;
+    private Patient patient;
+    private Doctor doctor;
+
+}
